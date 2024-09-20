e58f505c30c94ee0a09dacac00a77e65c03eb5e6
Ritex12in <riteshsk001@gmail.com>
2024-09-20 23:35:11 +0530

    Finalising search screen

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -3,6 +3,7 @@ import 'package:first_flutter_app/base/res/styles/app_styles.dart';
 import 'package:first_flutter_app/screens/search/widgets/app_text_icon.dart';
 import 'package:first_flutter_app/screens/search/widgets/app_ticket_tabs.dart';
 import 'package:first_flutter_app/screens/search/widgets/find_tickets.dart';
+import 'package:first_flutter_app/screens/search/widgets/ticket_promotion.dart';
 import 'package:flutter/material.dart';
 
 import '../../base/widgets/app_double_text.dart';
@@ -12,7 +13,6 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppStyles.bgColor,
       body: ListView(
@@ -40,45 +40,7 @@ class SearchScreen extends StatelessWidget {
             func: () => Navigator.pushNamed(context, "/all_tickets"),
           ),
           const SizedBox(height: 15,),
-          Row(
-            children: [
-              Container(
-                width: size.width*.42,
-                height: 405,
-                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade200,
-                      blurRadius: 1,
-                      spreadRadius: 2,
-                    )
-                  ]
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: const DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(AppMedia.planeSit),
-                        )
-                      ),
-                    ),
-                    const SizedBox(height: 12,),
-                    Text(
-                      "20% discount on early booking of this flight. Don't miss",
-                      style: AppStyles.headLineStyle2,
-                    )
-                  ],
-                )
-              )
-            ],
-          )
+          const TicketPromotion(),
         ],
       ),
     );
